92c860db03ced995be04bfff6403817fc69f0998
devanscreate <devanregiana12345@gmail.com>
2025-01-03 00:06:49 +0700

    Membuat Migration Fixes #1

---
+++ database/migrations/2024_01_16_055158_create_participant_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participant_students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('nama');
+            $table->string('nisn');
+            $table->string('tanggal_lahir');
+            $table->string('alamat_lengkap');
+            $table->string('asal_sekolah');
+            $table->float('nilai_raport_s1');
+            $table->float('nilai_raport_s2');
+            $table->float('nilai_raport_s3');
+            $table->float('nilai_raport_s4');
+            $table->float('nilai_raport_s5');
+            $table->string('file_raport');
+            $table->string('status')->default('diproses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participant_students');
+    }
+};
---
+++ database/migrations/2024_01_23_161236_create_wali_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wali_students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('nama_wali');
+            $table->string('alamat_wali');
+            $table->string('pekerjaan_wali');
+            $table->string('status_wali');
+            $table->string('nohp_wali');
+            $table->double('penghasilan_wali');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wali_students');
+    }
+};
